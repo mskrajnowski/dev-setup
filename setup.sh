5c64667eb9e5f38161e57ce1be215cb8218e0f94
Marek Skrajnowski <mskrajnowski@users.noreply.github.com>
2019-03-18 21:43:25 +0100

    Fix: setup.sh apt confirmation prompts

--- setup.sh
+++ setup.sh
@@ -2,14 +2,14 @@ REPO_PATH=$HOME/.dev-setup
 REPO_URL=git@github.com:mskrajnowski/dev-setup.git
 
 echo "Installing Git and apt-add-repository..."
-sudo apt-get install git software-properties-common
+sudo apt-get install -y git software-properties-common
 
 echo "Adding Ansible PPA..."
-sudo apt-add-repository ppa:ansible/ansible
+sudo apt-add-repository -y ppa:ansible/ansible
 sudo apt-get update
 
 echo "Installing Ansible..."
-sudo apt-get install ansible
+sudo apt-get install -y ansible
 
 echo "Cloning $REPO_URL..."
 git clone $REPO_URL $REPO_PATH
